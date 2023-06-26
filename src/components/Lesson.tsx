8c1c4d9228995f1855e20f507c36001345334100
Bruno Almeida <brunoalmeidasantos93@gmail.com>
2023-06-26 14:31:37 -0300

    Aplicação com Redux

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,16 +1,29 @@
-import { Video } from "lucide-react";
+import { PlayCircle, Video } from "lucide-react";
 
 interface LessonProps {
   title: string
   duration: string
+  isCurrent?: boolean
+  onPLay: () => void
 }
 
-export function Lesson({ title, duration }: LessonProps) {
+export function Lesson({ title, duration, onPLay, isCurrent = false }: LessonProps) {
   return (
-    <button className="flex items-center gap-3 text-sm text-zinc-400">
-      <Video className="w-4 h-4 text-zinc-500" />
+    <button
+      onClick={onPLay}
+      data-active={isCurrent}
+      disabled={isCurrent}
+      className="flex items-center gap-3 text-sm text-zinc-400 data-[active=true]:text-emerald-400 enabled:hover:text-zinc-100"
+    >
+      {isCurrent ? (
+        <PlayCircle className="h-4 w-4 text-emerald-400" />
+      ) : (
+        <Video className="w-4 h-4 text-zinc-500" />
+      )}
       <span>{title}</span>
-      <span className="ml-auto font-mono text-xs text-zinc-500">{duration}</span>
+      <span className="ml-auto font-mono text-xs text-zinc-500">
+        {duration}
+      </span>
     </button>
   )
 }
